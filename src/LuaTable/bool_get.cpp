0a2ed816a99be7aff404045d2caf257b704d5757
scongebop <littlefast2@gmail.com>
2023-05-26 16:09:56 +0800

    Removed misinformation and the "mistake" was not a mistake, so it was reverted

--- src/LuaTable/bool_get.cpp
+++ src/LuaTable/bool_get.cpp
@@ -8,14 +8,14 @@ bool LuaTable::get(const char* field)
 	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self()) // If the value of this reference is in the Lua registry
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
 		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, this->position);
 		// this->position[field]
 	}
-	else // If the value of this reference is on the Lua stack
+	else // If the value of this reference is in the Lua registry
 	{
 		//
 		lua_pushstring(this->thread, field);
@@ -38,14 +38,14 @@ bool LuaTable::get(int index)
 	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self()) // If the value of this reference is in the Lua registry
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
 		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, this->position);
 		// this->position[index]
 	}
-	else // If the value of this reference is on the Lua stack
+	else // If the value of this reference is in the Lua registry
 	{
 		//
 		lua_pushinteger(this->thread, index);
--- src/LuaTable/bool_set.cpp
+++ src/LuaTable/bool_set.cpp
@@ -8,21 +8,21 @@ void LuaTable::set(const char* field, const bool& value)
 	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self()) // If the value of this reference is in the Lua registry
-	{
-		//
-		lua_pushstring(this->thread, field);
-		lua_pushboolean(this->thread, value);
-		lua_settable(this->thread, -3);
-		// LUA_REGISTRY[this->key][field] = value
-	}
-	else // If the value of this reference is on the Lua stack
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
 		//
 		lua_pushstring(this->thread, field);
 		lua_pushboolean(this->thread, value);
 		lua_settable(this->thread, this->position);
 		// this->position[field] = value
+	}
+	else // If the value of this reference is in the Lua registry
+	{
+		//
+		lua_pushstring(this->thread, field);
+		lua_pushboolean(this->thread, value);
+		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][field] = value
 	}
 
 	// cleanup
@@ -35,21 +35,21 @@ void LuaTable::set(int index, const bool& value)
 	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self()) // If the value of this reference is in the Lua registry
-	{
-		//
-		lua_pushinteger(this->thread, index);
-		lua_pushboolean(this->thread, value);
-		lua_settable(this->thread, -3);
-		// LUA_REGISTRY[this->key][index] = value
-	}
-	else // If the value of this reference is on the Lua stack
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
 		//
 		lua_pushinteger(this->thread, index);
 		lua_pushboolean(this->thread, value);
 		lua_settable(this->thread, this->position);
 		// this->position[index] = value
+	}
+	else // If the value of this reference is in the Lua registry
+	{
+		//
+		lua_pushinteger(this->thread, index);
+		lua_pushboolean(this->thread, value);
+		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][index] = value
 	}
 
 	// cleanup
