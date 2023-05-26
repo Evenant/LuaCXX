348538c85fa76a89d6712d0aee8e4298ee618233
scongebop <littlefast2@gmail.com>
2023-05-26 15:37:44 +0800

    Cut out some dead code

--- src/LuaTable.cpp
+++ src/LuaTable.cpp
@@ -2,70 +2,14 @@
 #include "../LuaCXX.hpp"
 #include <cstddef>
 #include <cstdlib>
-#include <lua.h>
 #include <vector>
 
 using namespace LuaCXX;
 
 static Type _get_type(lua_State* L);
 
-static void clone_values(lua_State* L, int from, int to)
-{
-	lua_pushnil(L);  /* first key */
-    while (lua_next(L, from) != 0) {
-		constexpr int key = -2;
-		constexpr int value = -1;
 
 
-
-		if (lua_isstring(L, key))
-		{
-			const char* key_s = lua_tostring(L, key);
-			lua_getfield(L, from, key_s);
-			lua_setfield(L, to, key_s);
-
-			lua_pushstring(L, key_s);
-			lua_insert(L, key);
-
-		}
-		
-    	lua_pop(L, 1);
-    }
-
-	for (int i = 0; i < lua_objlen(L, from);i++)
-	{
-		lua_rawgeti(L, from, i+1);
-		lua_rawseti(L, to, i+1);
-	}
-}
-/*
-void LuaTable::add_field()
-{
-	this->field_count++;
-	lua_createtable(this->thread, this->index_count, this->field_count);
-
-	int new_table = lua_gettop(this->thread);
-
-	clone_values(this->thread, this->position, new_table);
-
-	lua_insert(this->thread, this->position);
-	lua_settop(this->thread, this->position);
-
-}
-
-void LuaTable::add_index()
-{
-	this->index_count++;
-	lua_createtable(this->thread, this->index_count, this->field_count);
-
-	int new_table = lua_gettop(this->L);
-
-	clone_values(this->thread, this->position, new_table);
-
-	lua_insert(this->thread, this->position);
-	lua_settop(this->thread, this->position);
-}
-*/
 LuaTable::LuaTable(lua_State* thread) : LuaRef(thread)
 {
 	lua_pushlightuserdata(this->thread, this->key);
