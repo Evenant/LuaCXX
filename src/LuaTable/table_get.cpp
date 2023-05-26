872a4a980f00329725a3a72d646776306b545837
scongebop <littlefast2@gmail.com>
2023-05-26 16:48:59 +0800

    Added comments demonstrating how things work

--- src/LuaTable/table_get.cpp
+++ src/LuaTable/table_get.cpp
@@ -5,22 +5,28 @@ using namespace LuaCXX;
 template <>
 LuaTable LuaTable::get(const char* field)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) //  If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, this->position);
+		// this->position[field] 
 	}
-	else
+	else //  If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][field]
 	}
 
 	LuaTable* tb = new LuaTable(this->thread);
 	tb->set_self(this->thread);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return *tb;
@@ -29,22 +35,28 @@ LuaTable LuaTable::get(const char* field)
 template <>
 LuaTable LuaTable::get(int index)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) //  If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, this->position);
+		// this->position[index]
 	}
-	else 
+	else //  If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][index]
 	}
 
 	LuaTable* tb = new LuaTable(this->thread);
 	tb->set_self(this->thread);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return *tb;
