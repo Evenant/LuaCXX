75f207ccdfbacdf6e2415f59e9a1a50ed2998ee1
scongebop <littlefast2@gmail.com>
2023-05-26 16:28:57 +0800

    Added comments demonstrating how stuff works

--- src/LuaTable/string_get.cpp
+++ src/LuaTable/string_get.cpp
@@ -5,21 +5,28 @@ using namespace LuaCXX;
 template <>
 const char* LuaTable::get(const char* field)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, this->position);
+		// this->position[field]
 	}
-	else
+	else // If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][field]
 	}
-
+	
+	// get the string
 	const char* s = lua_tostring(this->thread, -1);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return s;
@@ -28,21 +35,28 @@ const char* LuaTable::get(const char* field)
 template <>
 const char* LuaTable::get(int index)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self())// If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, this->position);
+		// this->position[index]
 	}
-	else 
+	else // If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][index]
 	}
 
+	// get the string
 	const char* s = lua_tostring(this->thread, -1);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return s;
--- src/LuaTable/string_set.cpp
+++ src/LuaTable/string_set.cpp
@@ -5,41 +5,53 @@ using namespace LuaCXX;
 template <>
 void LuaTable::set(const char* field, const char*& value)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self())// If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_pushstring(this->thread, value);
 		lua_settable(this->thread, this->position);
+		// this->position[field] = value
 	}
-	else 
+	else // If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_pushstring(this->thread, value);
 		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][field] = value
 	}
 
+	// cleanup
 	lua_settop(this->thread, bf);
 }
 
 template <>
 void LuaTable::set(int index, const char*& value)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_pushstring(this->thread, value);
 		lua_settable(this->thread, this->position);
+		// this->position[index] = value
 	}
-	else 
+	else // If the value of this reference is on the Lua registry
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_pushstring(this->thread, value);
 		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][index] = value
 	}
 
+	// cleanup
 	lua_settop(this->thread, bf);
 }
