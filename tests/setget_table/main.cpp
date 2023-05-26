3013859354b3b67e03d09ac389ded9ab81cc7913
scongebop <littlefast2@gmail.com>
2023-05-26 14:17:09 +0800

    Updated test

--- tests/setget_table/main.cpp
+++ tests/setget_table/main.cpp
@@ -1,7 +1,6 @@
 #include "../../LuaCXX.hpp"
 #include "../testing.hpp"
-
-void traverse_table(LuaCXX::LuaTable& tb);
+#include <iostream>
 
 int main()
 {
@@ -15,43 +14,41 @@ int main()
 
 		LuaTable car_table = luat.new_table();
 
+		const char* carwheels_table_k = "wheels";
 		{
-			LuaTable carwheels_table = luat.new_table(0, 4);
+			LuaTable carwheels_table = luat.new_table();
 
-			carwheels_table.set_string(carwheels_table.array_size(), "Wheel1");
-			carwheels_table.set_string(carwheels_table.array_size(), "Wheel2");
-			carwheels_table.set_string(carwheels_table.array_size(), "Wheel3");
-			carwheels_table.set_string(carwheels_table.array_size(), "Wheel4");
+			const char * Wheel1_k = "Wheel1";
+			carwheels_table.set(carwheels_table.get_top()+1, Wheel1_k);
 
-			car_table.set_table("wheels", carwheels_table);
-			cout << "After set table in wrapper\n";
+			const char * Wheel2_k = "Wheel2";
+			carwheels_table.set(carwheels_table.get_top()+1, Wheel2_k);
+
+			const char * Wheel3_k = "Wheel3";
+			carwheels_table.set(carwheels_table.get_top()+1, Wheel3_k);
+
+			const char * Wheel4_k = "Wheel4";
+			carwheels_table.set(carwheels_table.get_top()+1, Wheel4_k);
+
+			car_table.set(carwheels_table_k, carwheels_table);
 		}
-		cout << "After set table outside wrapper\n";
 
-		car_table.set_bool("has_driver", true);
-		car_table.set_number("km_h", 50.0);
+		const bool has_driver = false;
+		car_table.set((const char*)"has_driver", has_driver);
+		const double km_h = 50.0;
+		car_table.set((const char*)"km_h", km_h);
 
-		cout << "Before table traverse\n";
-		traverse_table(car_table);
-		cout << "After table traverse\n";
+		LuaTable carwheels_table = car_table.get<LuaTable>(carwheels_table_k);
 
+		for (int i = 1;i <= carwheels_table.get_top();i++)
+		{
+			std::cout << carwheels_table.get<const char*>(i) << std::endl;
+		}
 
+		std::cout << "this car goes at " << car_table.get<double>("km_h") << " kilometers per hour." << std::endl;
+
+		ASSERT((!car_table.get<bool>("has_driver")), "HEY THERE IS A THIEF IN THE CAR!");
 	}
 
 	TEST_END;
-}
-void traverse_table(LuaCXX::LuaTable& tb)
-{
-	auto v = tb.get_all_fields();
-
-	for (auto i = v.cbegin(); i != v.cend();++i)
-	{
-		std::cout << *i << std::endl;
-		if (tb.get_type(*i) == LuaCXX::Type::Table)
-		{
-			std::cout << "The above is a table\n";
-			auto sub_tb = tb.get_table(*i);
-			traverse_table(sub_tb);
-		}
-	}
 }
