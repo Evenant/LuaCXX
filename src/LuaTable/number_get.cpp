b6cb897be803ed8a6c224a6257d2999c12368160
scongebop <littlefast2@gmail.com>
2023-05-26 16:18:13 +0800

    Added some cmments demonstrating how things work

--- src/LuaTable/number_get.cpp
+++ src/LuaTable/number_get.cpp
@@ -5,21 +5,28 @@ using namespace LuaCXX;
 template <>
 double LuaTable::get(const char* field)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, this->position);
+		// this->position[field]
 	}
-	else
+	else // If the value of this reference is in the Lua registry
 	{
+		//
 		lua_pushstring(this->thread, field);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][field]
 	}
 
+	// get the number
 	double n = lua_tonumber(this->thread, -1);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return n;
@@ -28,21 +35,28 @@ double LuaTable::get(const char* field)
 template <>
 double LuaTable::get(int index)
 {
+	// for cleanup
 	const int bf = lua_gettop(this->thread);
 
-	if (!this->push_self())
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, this->position);
+		// this->positions[index]
 	}
-	else 
+	else // If the value of this reference is in the Lua registry
 	{
+		//
 		lua_pushinteger(this->thread, index);
 		lua_gettable(this->thread, -2);
+		// LUA_REGISTRY[this->key][index]
 	}
 
+	// get the number
 	double n = lua_tonumber(this->thread, -1);
 
+	// cleanup
 	lua_settop(this->thread, bf);
 
 	return n;
