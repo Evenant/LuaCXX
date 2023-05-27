68a84395406a8b76eed70ce797dfe09741e52962
scongebop <littlefast2@gmail.com>
2023-05-27 23:11:39 +0800

    Cloned the code from move_up to move_down, and the for loop now uses a constant int that gets defined before the loop runs

--- src/LuaTable/array_move.cpp
+++ src/LuaTable/array_move.cpp
@@ -4,7 +4,8 @@ using namespace LuaCXX;
 
 void LuaTable::move_up(int index)
 {
-	for (int i = this->get_top(); i >= index; i--)
+	const int top = this->get_top();
+	for (int i = top; i >= index; i--)
 	{
 		const LuaType t = this->get_type(i);
 		if (t == LuaType::Number)
@@ -78,9 +79,75 @@ void LuaTable::move_up(int index)
 
 void LuaTable::move_down(int index)
 {
-	// for cleanup
-	int bf = lua_gettop(this->thread);
+	const int top = this->get_top();
+	for (int i = index; i <= top; i--)
+	{
+		const LuaType t = this->get_type(i);
+		if (t == LuaType::Number)
+		{
+			const double v = this->get<double>(i+1);
+			this->set(i, v);
+		}
+		else if (t == LuaType::String)
+		{
+			const char* v = this->get<const char*>(i+1);
+			this->set(i, v);
+		}
+		else if (t == LuaType::Boolean)
+		{
+			bool v = this->get<bool>(i+1);
+			this->set(i, v);
+		}
+		/* TODO
+			Implement a way for tables to set a LuaThread via `LuaTable::set`, and then
+			come back here and use it here
+		
+		else if (t == LuaType::Thread)
+		{
+		}
+		*/
+		else if (t == LuaType::Table)
+		{
+			LuaTable v = this->get<LuaTable>(i+1);
+			this->set(i, v);
+		}
+		/* TODO
+			Implement a way to create Userdata objects from a C++ class.
 
-	// cleanup
-	lua_settop(this->thread, bf);
+			after that:
+
+			Implement a way for tables to set a Userdata via `LuaTable::set`, and then
+			come back here and use it here.
+
+		else if (t == LuaType::Userdata)
+		{
+		}
+		*/
+		/* TODO
+			Implement a way for tables to set a void* pointer ( aka a Lua light userdata ) via `LuaTable::set`, and then
+			come back here and use it here
+
+		else if (t == LuaType::LightUserdata)
+		{
+		}
+		*/
+		/* TODO
+			Implement a way for LuaCXX to create functions that function similarly to Lua C Functions
+
+			after that:
+
+			Implement a way for tables to set a LuaCXX Lua function via `LuaTable::set`, and then
+			come back here and use it here
+
+		else if (t == LuaType::Function)
+		{
+
+		}
+		*/
+		else
+		{
+			this->set_nil(i);
+		}
+
+	}
 }
