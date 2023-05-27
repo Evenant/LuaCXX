b9ae1214591629b74ce0562f5838daa6aa3ab54b
scongebop <littlefast2@gmail.com>
2023-05-27 23:05:16 +0800

    LuaTable::move_up now using the available setget methods, cut out a large chunk of useless code and added TODO's around

--- src/LuaTable/array_move.cpp
+++ src/LuaTable/array_move.cpp
@@ -4,77 +4,76 @@ using namespace LuaCXX;
 
 void LuaTable::move_up(int index)
 {
-	
 	for (int i = this->get_top(); i >= index; i--)
 	{
 		const LuaType t = this->get_type(i);
-		if (t == LuaType::Nil)
+		if (t == LuaType::Number)
 		{
-			this->set_nil(i+1);
-		}
-		else if (t == LuaType::Number)
-		{
-
+			const double v = this->get<double>(i);
+			this->set(i+1, v);
 		}
 		else if (t == LuaType::String)
 		{
-
+			const char* v = this->get<const char*>(i);
+			this->set(i+1, v);
 		}
 		else if (t == LuaType::Boolean)
 		{
-
+			bool v = this->get<bool>(i);
+			this->set(i+1, v);
 		}
+		/* TODO
+			Implement a way for tables to set a LuaThread via `LuaTable::set`, and then
+			come back here and use it here
+		
 		else if (t == LuaType::Thread)
 		{
-
 		}
-		else if (t == LuaType::None)
-		{
-			
-		}
+		*/
 		else if (t == LuaType::Table)
 		{
-
+			LuaTable v = this->get<LuaTable>(i);
+			this->set(i+1, v);
 		}
+		/* TODO
+			Implement a way to create Userdata objects from a C++ class.
+
+			after that:
+
+			Implement a way for tables to set a Userdata via `LuaTable::set`, and then
+			come back here and use it here.
+
 		else if (t == LuaType::Userdata)
 		{
-
 		}
+		*/
+		/* TODO
+			Implement a way for tables to set a void* pointer ( aka a Lua light userdata ) via `LuaTable::set`, and then
+			come back here and use it here
+
 		else if (t == LuaType::LightUserdata)
 		{
-
 		}
+		*/
+		/* TODO
+			Implement a way for LuaCXX to create functions that function similarly to Lua C Functions
+
+			after that:
+
+			Implement a way for tables to set a LuaCXX Lua function via `LuaTable::set`, and then
+			come back here and use it here
+
 		else if (t == LuaType::Function)
 		{
 
 		}
+		*/
+		else
+		{
+			this->set_nil(i+1);
+		}
 
 	}
-
-	return; // dont run the code after this
-			// Essentially im saving the code below incase it is useful.
-	
-	// for cleanup
-	int bf = lua_gettop(this->thread);
-
-	// position of the table we are working with
-	int tb = 0;
-
-	if (!this->push_self()) // If the value of this reference is on the Lua stack
-	{
-		tb = this->position;
-	}
-	else // If the value of this reference is on the Lua registry
-	{
-		// The table just pushed itself to the top of the stack
-		// so do this
-		tb = lua_gettop(this->thread);
-	}
-
-	
-	
-	// cleanup
-	lua_settop(this->thread, bf);
 }
 
 void LuaTable::move_down(int index)
