034c87e33a518d23ad6ef38f33d6be09d26460f5
scongebop <littlefast2@gmail.com>
2023-05-27 18:25:19 +0800

    Actually implemented `set_nil`

--- src/LuaTable/nil_set.cpp
+++ src/LuaTable/nil_set.cpp
@@ -2,5 +2,58 @@
 
 using namespace LuaCXX;
 
+template<>
+void LuaTable::set_nil(int index)
+{
+	// for cleanup
+	int bf = lua_gettop(this->thread);
 
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
+	{
+		//
+		lua_pushinteger(this->thread, index);
+		lua_pushnil(this->thread);
+		lua_settable(this->thread, this->position);
+		// this->position[index] = nil
+	}
+	else
+	{
+		// 
+		lua_pushinteger(this->thread, index);
+		lua_pushnil(this->thread);
+		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][index] = nil
 
+	}
+
+	//cleanup
+	lua_settop(this->thread, bf);
+}
+
+template<>
+void LuaTable::set_nil(const char* field)
+{
+	// for cleanup
+	int bf = lua_gettop(this->thread);
+
+	if (!this->push_self()) // If the value of this reference is on the Lua stack
+	{
+		//
+		lua_pushstring(this->thread, field);
+		lua_pushnil(this->thread);
+		lua_settable(this->thread, this->position);
+		// this->position[field] = nil
+	}
+	else
+	{
+		// 
+		lua_pushstring(this->thread, field);
+		lua_pushnil(this->thread);
+		lua_settable(this->thread, -3);
+		// LUA_REGISTRY[this->key][field] = nil
+
+	}
+
+	//cleanup
+	lua_settop(this->thread, bf);
+}
